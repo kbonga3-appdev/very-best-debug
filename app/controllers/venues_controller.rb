c4d336d84b55afcbef8e4d7e7f955be2670e2565
kbonga2-appdev <stuwrkr-kbonga2@mx.uillinois.edu>
2023-03-11 21:44:14 +0000

    cus-venue trace

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,23 +1,24 @@
 class VenuesController < ApplicationController
 
   def index
-    matching_venues = venue.all
-    venues = matching_venues.order(:created_at)
+    matching_venues = Venue.all
+    @venues = matching_venues.order(:created_at)
 
     render({ :template => "venue_templates/venue_list.html.erb" })
   end
 
   def show
     venue_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
+    matching_venues = Venue.where({ :id => venue_id }).at(0)
+    @the_venue = matching_venues
 
     render({ :template => "venue_templates/details.html.erb" })
   end
 
   def create
-    @venue = Venue.new
-    venue.address = params.fetch("query_address")
+    venue = Venue.new
+    # venue.address = params.fetch("query_username")
+     venue.address = params.fetch("query_address")
     venue.name = params.fetch("name")
     venue.neighborhood = params.fetch("neighborhood")
     venue.save
@@ -28,9 +29,10 @@ class VenuesController < ApplicationController
   def update
     the_id = params.fetch("venue_id")
 
-    @venue = Venue.where({ :id => the_id })
+    # @venue = Venue.where({ :id => the_id })
+    venue = Venue.where({ :id => the_id }).at(0)
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
+    venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
     
@@ -39,7 +41,7 @@ class VenuesController < ApplicationController
 
   def destroy
     the_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => the_id })
+    matching_venues = Venue.where({ :id => the_id }).at(0)
     venue = matching_venues
     venue.destroy
 
